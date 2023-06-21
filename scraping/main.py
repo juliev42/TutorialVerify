5111d0934abb0253e3f87fb67d74fb86e52c809b
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-06-21 02:24:45 -0400

    --- updates

--- scraping/main.py
+++ scraping/main.py
@@ -1,1 +1,13 @@
+import os
+import psycopg2
 from scraper import *
+
+#  get DATABASE_URL environment variable
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL)
+
+# LANGCHAIN scraping logic
+
+# close the connection
+conn.close()
