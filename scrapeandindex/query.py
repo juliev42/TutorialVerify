98987a987d44e70c6dc41354e2a9a0229cbfdae5
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-06-22 19:34:55 -0400

    Update query.py

--- scrapeandindex/query.py
+++ scrapeandindex/query.py
@@ -1,5 +1,11 @@
 import psycopg2
 
+# connect to the database
+# conn = psycopg2.connect(DATABASE_URL)
+
+# create a cursor
+# cur = conn.cursor()
+
 def select_row_by_index(index, tablename, columnname):
     with conn.cursor() as cur:
         cur.execute(f"SELECT {columnname} FROM {tablename} LIMIT 1 OFFSET %s", (index,))
@@ -7,19 +13,27 @@ def select_row_by_index(index, tablename, columnname):
         return row
     
 # get the heading in the 5th row of the headings table
-heading = select_row_by_index(500, 'headings', '*')
-print(heading)
+# heading = select_row_by_index(500, 'headings', '*')
+# print(heading)
 
 # get the url at the urlid in the headings table
-url = select_row_by_index(heading[0], 'urls', '*')
-print(url)
+# url = select_row_by_index(heading[0], 'urls', '*')
+# print(url)
 
-def get_url_from_heading_idx(heading_idx):
-    heading = select_row_by_index(heading_idx, 'headings', 'urlid')
+def get_url_by_headingid(rowid):
+    heading = cur.execute("SELECT urlid FROM headings WHERE rowid = %s", (rowid,))
     url = select_row_by_index(heading[0], 'urls', '*')
     return url # list of items related to the url
 
-def get_heading_by_rowid(rowid):
+def get_heading_by_rowid(rowid): # row id is the same as pineconeid here
     cur.execute("SELECT * FROM headings WHERE rowid = %s", (rowid,))
     heading = cur.fetchone()
     return heading # list of items related to the heading
+
+# print(get_heading_by_rowid(500))
+
+# close the cursor
+# cur.close()
+
+# close the connection
+# conn.close()
