88af0414e1701f61039bb322d20bf2a79ca75887
Morgan Mendis <morganandrewmendis@gmail.com>
2023-06-22 22:07:19 -0500

    Updated the streamlit app to connect with the pinecone-langchain object that queries the pinecone DB

--- streamlit/syllabot_verify.py
+++ streamlit/syllabot_verify.py
@@ -2,14 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+import sys
 from streamlit_chat import message
 import openai
 
+# Load environment variables
 from dotenv import load_dotenv
-
 load_dotenv()
 
 ##TODO use langchain pinecone client to implement flow here
+# Add the current directory to the syspath
+currentdir = os.getcwd()
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+# Import the custom python module for interacting with Pinecone and OpenAI
+import components.pinecone_langchain as plc
+pilang = plc.LangChainPineconeClient()
 
 ## 
 st.title('Syllabot Verification')
@@ -33,7 +42,7 @@ with st.form("chat_input", clear_on_submit=True):
     a, b = st.columns([4, 1])
     user_input = a.text_input(
         label="Your message:",
-        placeholder="What would you like to say?",
+        placeholder="What would you like to ask about LangChain?",
         label_visibility="collapsed",
     )
     b.form_submit_button("Send", use_container_width=True)
@@ -46,7 +55,9 @@ if user_input and openai_api_key:
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": user_input})
     message(user_input, is_user=True)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
+    #response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = pilang.ask_with_context(user_input)
+    #msg = response.choices[0].message
+    msg = {"role": "assistant","content":response}
     st.session_state.messages.append(msg)
-    message(msg.content)
+    message(msg['content'])
