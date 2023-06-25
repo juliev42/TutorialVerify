00524cad88051c501efb0342e733c59ce25ba954
Morgan Mendis <morganandrewmendis@gmail.com>
2023-06-25 10:48:55 -0500

    Adjusted the NavBar and hid the submit  button once a user made a submission

--- streamlit/syllabot_verify.py
+++ streamlit/syllabot_verify.py
@@ -20,37 +20,54 @@ sys.path.append(parentdir)
 import components.pinecone_langchain as plc
 pilang = plc.LangChainPineconeClient()
 
-## 
-st.title('Syllabot Verification')
-
+# Try to get the OpenAI API key from the environment variables
 try:
     openai_api_key = os.getenv('OPENAI_API_KEY')
 except:
     openai_api_key = None
     raise Exception('OPENAI_API_KEY not found in environment variables')
 
+## Begin Streamlit App
+# Configure the page to be wide
+st.set_page_config(layout='wide')
+# Set the title of the app
+st.title('Syllabot Verification')
+
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 with st.sidebar:
     st.markdown("""
-    ## About this App
+     ## About this App:
     This app is a demo of using Pinecone to implement a chatbot. That processes a user's  course content and determines if it needs to be updated.
+    
+    ## How to use it:
+    Please submit text that you would like to verify about LangChain.
+    Take passages from Medium articles, or other sources and submit them to the chatbot. The chatbot will then verify the content and provide a response.
+    *Sample Articles:*
+    - https://medium.com/databutton/getting-started-with-langchain-a-powerful-tool-for-working-with-large-language-models-286419ba0842
+    - https://towardsdatascience.com/a-gentle-intro-to-chaining-llms-agents-and-utils-via-langchain-16cd385fca81
     """)
 
-with st.form("chat_input", clear_on_submit=True):
-    a, b = st.columns([4, 1])
-    user_input = a.text_input(
-        label="Your message:",
-        placeholder="What would you like to ask about LangChain?",
-        label_visibility="collapsed",
-    )
-    b.form_submit_button("Send", use_container_width=True)
 
 for msg in st.session_state["messages"]:
     message(msg["content"], is_user=msg["role"] == "user")
 
+# Intialize a holder for the input box
+holder = st.empty()
+# Use the holder to create a form which we will hide later after the user submits
+with holder.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([4, 1])
+    user_input = a.text_input(
+        label="Your message:",
+        placeholder="What would you like to verify about LangChain?",
+        label_visibility="collapsed",
+    )
+    b.form_submit_button("Send", use_container_width=True)
+
 if user_input and openai_api_key:
+    holder.empty() # Remove the input box from the app
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": user_input})
     message(user_input, is_user=True)
